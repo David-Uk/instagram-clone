0beb359124d24ece6c32a62d3fce2e652b1a369c
David Ukelere <david.ukelere291@gmail.com>
2021-01-21 20:04:35 +0100

    Basic setup completed

---
+++ backend/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const MONGO_URL = 'mongodb://127.0.0.1:27017/';
+
+mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+mongoose.connection.on('open', () => {
+    console.log('COnnected to database successfully')
+})
+mongoose.connection.on('error', err => {
+    console.log(`Error ${err} encountered`);
+})
+
+const db =
+
+    app.get('/', (req, res) => {
+        res.send('Hello World');
+    })
+
+app.listen(PORT, () => {
+    console.log(`Started on port ${PORT}`);
+})
