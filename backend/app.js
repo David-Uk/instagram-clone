d5c8e1fba6620f18f6f44e60fa00ab3b11aa24f8
David Ukelere <david.ukelere291@gmail.com>
2021-01-22 23:26:27 +0100

    Created user model

--- backend/app.js
+++ backend/app.js
@@ -5,12 +5,15 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URL = 'mongodb://127.0.0.1:27017/';
 
+// Import user model
+require('./models/User');
+
 mongoose.connect(MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
 mongoose.connection.on('open', () => {
-    console.log('COnnected to database successfully')
+    console.log('Connected to database successfully')
 })
 mongoose.connection.on('error', err => {
     console.log(`Error ${err} encountered`);
