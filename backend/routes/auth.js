c6a4d016c4c1f8a651edca46a108d61379602b3f
David Ukelere <david.ukelere291@gmail.com>
2021-01-22 23:06:59 +0100

    Created signin route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -31,4 +31,20 @@ router.post('/signup', (req, res) => {
 
 })
 
+router.post('/signin', (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) return res.status(422).json({ error: 'Please provide required login details' })
+    User.findOne({ email: email })
+        .then(savedUser => {
+            if (!savedUser) return res.status(422).json({ error: 'Invalid email or password' })
+            bcrypt.compare(password, savedUser.password)
+                .then(doMatch => {
+                    if (doMatch) return res.json({ message: 'Login successful' })
+                    else return res.status(422).json({ error: 'Invalid email or password' })
+                })
+                .catch(err => console.log(err))
+        })
+        .catch(err => console.log(err))
+})
+
 module.exports = router;
